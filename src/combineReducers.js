f8c240d2b5951205a0a1112c8d8dbb97ee7a9a94
bashkernel <zhengwusjtu@gmail.com>
2017-12-13 17:34:23 +0800

    Update combineReducers.js

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -100,11 +100,14 @@ function assertReducerShape(reducers) {
 }
 
 /**
+ * combineReducers函数作用：把一个对象，对象的value是一个个不同的reduce函数，合并成一个reduce函数。
+ * 内部调用每个reduce函数，合并结果到同一个state对象，对应的key值是传过来的reduce函数名称。
  * Turns an object whose values are different reducer functions, into a single
  * reducer function. It will call every child reducer, and gather their results
  * into a single state object, whose keys correspond to the keys of the passed
  * reducer functions.
  *
+ * 
  * @param {Object} reducers An object whose values correspond to different
  * reducer functions that need to be combined into one. One handy way to obtain
  * it is to use ES6 `import * as reducers` syntax. The reducers may never return
@@ -112,12 +115,14 @@ function assertReducerShape(reducers) {
  * if the state passed to them was undefined, and the current state for any
  * unrecognized action.
  *
+ * 返回值：一个总reducer，内部包含所有子reducer
  * @returns {Function} A reducer function that invokes every reducer inside the
  * passed object, and builds a state object with the same shape.
  */
 export default function combineReducers(reducers) {
   const reducerKeys = Object.keys(reducers)
   const finalReducers = {}
+  //过滤非函数reduce，返回finalReducers
   for (let i = 0; i < reducerKeys.length; i++) {
     const key = reducerKeys[i]
 
@@ -144,7 +149,7 @@ export default function combineReducers(reducers) {
   } catch (e) {
     shapeAssertionError = e
   }
-
+  //最终返回一个函数
   return function combination(state = {}, action) {
     if (shapeAssertionError) {
       throw shapeAssertionError
@@ -164,18 +169,22 @@ export default function combineReducers(reducers) {
 
     let hasChanged = false
     const nextState = {}
+    //遍历reducer数组，执行每个reducer的方法
     for (let i = 0; i < finalReducerKeys.length; i++) {
       const key = finalReducerKeys[i]
       const reducer = finalReducers[key]
       const previousStateForKey = state[key]
+      //传入旧state，action，得到新state
       const nextStateForKey = reducer(previousStateForKey, action)
       if (typeof nextStateForKey === 'undefined') {
         const errorMessage = getUndefinedStateErrorMessage(key, action)
         throw new Error(errorMessage)
       }
       nextState[key] = nextStateForKey
+      //判断状态是否发生变化
       hasChanged = hasChanged || nextStateForKey !== previousStateForKey
     }
+        //返回值处理后的state
     return hasChanged ? nextState : state
   }
 }
