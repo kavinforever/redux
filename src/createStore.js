287e33c37ada5dc3b24e18e00588783634003b2d
bashkernel <zhengwusjtu@gmail.com>
2017-12-13 15:48:56 +0800

    Update createStore.js

--- src/createStore.js
+++ src/createStore.js
@@ -4,36 +4,45 @@ import ActionTypes from './utils/actionTypes'
 import isPlainObject from './utils/isPlainObject'
 
 /**
+ * 创建一个redux的store来维护全局的状态树,唯一改变状态树里的数据的方法是调用dispatch方法
  * Creates a Redux store that holds the state tree.
  * The only way to change the data in the store is to call `dispatch()` on it.
  *
+ * 一个app里应该只有一个全局的store，为了细化不同的actions对应state的不同部分的状态树，
+ * 可以通过combineReduces方法来组合多个reduces到同一个reduce
  * There should only be a single store in your app. To specify how different
  * parts of the state tree respond to actions, you may combine several reducers
  * into a single reducer function by using `combineReducers`.
  *
+ * reduce参数：一个函数返回下一个状态树，根据给定的当前状态树和对应的action
  * @param {Function} reducer A function that returns the next state tree, given
  * the current state tree and the action to handle.
  *
+ * preloadedState 初始化状态，你可以从一个server端获取，也可以从前一个序列号用户session里获取
+ * 如果是使用combineReducers产生的reduce函数，preloaderState必须是一个和combineReduces的key一致的对象
  * @param {any} [preloadedState] The initial state. You may optionally specify it
  * to hydrate the state from the server in universal apps, or to restore a
  * previously serialized user session.
  * If you use `combineReducers` to produce the root reducer function, this must be
  * an object with the same shape as `combineReducers` keys.
  *
+ * enhance store的扩展函数,支持中间件扩展和时间穿梭、持续化等等。
  * @param {Function} [enhancer] The store enhancer. You may optionally specify it
  * to enhance the store with third-party capabilities such as middleware,
  * time travel, persistence, etc. The only store enhancer that ships with Redux
  * is `applyMiddleware()`.
  *
+ * 返回一个store数据对象，让你能够获取state、dispatch actions和订阅能够改变state数据
  * @returns {Store} A Redux store that lets you read the state, dispatch actions
  * and subscribe to changes.
  */
 export default function createStore(reducer, preloadedState, enhancer) {
+  // 判断接受的参数个数，来指定 reducer 、 preloadedState 和 enhancer
   if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
     enhancer = preloadedState
     preloadedState = undefined
   }
-
+  // 如果 enhancer 存在并且适合合法的函数，那么调用 enhancer，并且终止当前函数执行
   if (typeof enhancer !== 'undefined') {
     if (typeof enhancer !== 'function') {
       throw new Error('Expected the enhancer to be a function.')
@@ -41,17 +50,21 @@ export default function createStore(reducer, preloadedState, enhancer) {
 
     return enhancer(createStore)(reducer, preloadedState)
   }
-
+  //如果reduce非函数，那么抛出异常
   if (typeof reducer !== 'function') {
     throw new Error('Expected the reducer to be a function.')
   }
-
+  // 储存当前的 currentReducer
   let currentReducer = reducer
+  // 储存当前的状态
   let currentState = preloadedState
+  // 储存当前的监听函数列表
   let currentListeners = []
+  // 储存下一个监听函数列表
   let nextListeners = currentListeners
   let isDispatching = false
-
+  
+  // 这个函数可以根据当前监听函数的列表生成新的下一个监听函数列表引用
   function ensureCanMutateNextListeners() {
     if (nextListeners === currentListeners) {
       nextListeners = currentListeners.slice()
@@ -60,7 +73,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
 
   /**
    * Reads the state tree managed by the store.
-   *
+   * 从store当中获取当前的state状态树
    * @returns {any} The current state tree of your application.
    */
   function getState() {
@@ -76,6 +89,11 @@ export default function createStore(reducer, preloadedState, enhancer) {
   }
 
   /**
+   * 添加一个事件监听，subscribe接收一个listener，它的作用是给store添加监听函数。
+   * nextListeners储存了整个监听函数列表。 subscribe的返回值是一个unsubscribe，
+   * 是一个解绑函数，调用该解绑函数，会将已经添加的监听函数删除，该监听函数处于一个闭包之中，
+   * 会一直存在，所以在解绑函数中能删除该监听函数。（由此可见redux源码设计的精巧，多处地方巧用闭包，精简了许多代码。）
+   * 
    * Adds a change listener. It will be called any time an action is dispatched,
    * and some part of the state tree may potentially have changed. You may then
    * call `getState()` to read the current state tree inside the callback.
@@ -138,6 +156,9 @@ export default function createStore(reducer, preloadedState, enhancer) {
   }
 
   /**
+   * dispatch接收一个参数action。代码会先调用createStore传入的参数reducer方法，
+   * reducer接受当前state和action，通过判断actionType，来做对应的操作，并返回最新的currentState。
+   * dispatch还会触发整个监听函数列表，所以最后整个监听函数列表都会按顺序执行一遍。dispatch返回值就是传入的action。
    * Dispatches an action. It is the only way to trigger a state change.
    *
    * The `reducer` function, used to create the store, will be called with the
@@ -163,31 +184,36 @@ export default function createStore(reducer, preloadedState, enhancer) {
    * return something else (for example, a Promise you can await).
    */
   function dispatch(action) {
+    //判断是否是一个对象
     if (!isPlainObject(action)) {
       throw new Error(
         'Actions must be plain objects. ' +
           'Use custom middleware for async actions.'
       )
     }
-
+    //判断action对象是否存在type的key
     if (typeof action.type === 'undefined') {
       throw new Error(
         'Actions may not have an undefined "type" property. ' +
           'Have you misspelled a constant?'
       )
     }
-
+    
+      // 如果正在 dispatch 则抛出错误
     if (isDispatching) {
       throw new Error('Reducers may not dispatch actions.')
     }
-
+    
+  // 对抛出 error 的兼容，但是无论如何都会继续执行 isDispatching = false 的操作
     try {
       isDispatching = true
+      // 使用 currentReducer 来操作传入 当前状态和action，放回处理后的状态
       currentState = currentReducer(currentState, action)
     } finally {
       isDispatching = false
     }
-
+    
+    //遍历 `nextListeners` 列表，调用每个监听函数；
     const listeners = (currentListeners = nextListeners)
     for (let i = 0; i < listeners.length; i++) {
       const listener = listeners[i]
@@ -198,6 +224,11 @@ export default function createStore(reducer, preloadedState, enhancer) {
   }
 
   /**
+   * replaceReducer是替换当前的reducer的函数，replaceReducer接受一个新的reducer，替换完成之后，
+   * 会执行 dispatch({ type: ActionTypes.INIT }) ，用来初始化store的状态。官方举出了三种replaceReducer的使用场景，分别是：
+   * 1.当你的程序要进行代码分割的时候
+   * 2.当你要动态的加载不同的reducer的时候
+   * 3.当你要实现一个实时reloading机制的时候
    * Replaces the reducer currently used by the store to calculate the state.
    *
    * You might need this if your app implements code splitting and you want to
@@ -217,6 +248,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
   }
 
   /**
+   * 内部调用接口
    * Interoperability point for observable/reactive libraries.
    * @returns {observable} A minimal observable of state changes.
    * For more information, see the observable proposal:
@@ -258,6 +290,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
   // When a store is created, an "INIT" action is dispatched so that every
   // reducer returns their initial state. This effectively populates
   // the initial state tree.
+  // 私有action
   dispatch({ type: ActionTypes.INIT })
 
   return {
